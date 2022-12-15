4969c50c4ead5f8f7f91d75701f95698ccb9de4c
Vryahn <b.ra@live.com.mx>
2022-12-14 23:55:00 -0600

    deleting frontEnd files

--- js/environment.js
+++
@@ -1,3 +0,0 @@
-const db = "https://to-do-list-dc76a-default-rtdb.firebaseio.com/to-do-list";
-
-export default db;
--- js/removePost.js
+++
@@ -1,7 +0,0 @@
-export const removePostMethod = (db, id) => {
-    fetch(`${db}/${id}.json`, {
-    method: "DELETE",
-  })
-    .then((res) => res.json())
-    // .then((data) => console.log("Elemento eliminado", data));
-};
