0a8865a9e3c4774d743df04693b541413fa393f5
sueenciso <susana.enciso@icloud.com>
2022-12-14 22:22:43 -0600

    added cors packege

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const cors=require("cors")
 const app = express();
 const apiRouter = require("./src/routes");
 const { logErrors, errorHandler } = require("./src/middlewares/errorHandler");
 const config = require("./src/lib/config");
 const db = require("./src/lib/db");
 
+app.use(cors());
 app.use(express.json());
 apiRouter(app);
 
 app.use(logErrors);
 app.use(errorHandler);
+
 
 app.get("/", (req, res) => {
   res.json({ message: "El API ya funciona" });
